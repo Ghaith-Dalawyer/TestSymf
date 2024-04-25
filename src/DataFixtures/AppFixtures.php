74640d99f3916532fa09a06aa748200b7211b6c5
Ghaith <mekluless@gmail.com>
2024-04-25 22:56:33 +0100

    Added all the Factories nad updated appFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,15 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Factory\EnseignantFactory;
+use App\Factory\MatiereFactory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        EnseignantFactory::createMany(3);
+        
+        MatiereFactory::createMany(10);
     }
 }
